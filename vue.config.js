37e89a2bca66cccc8463c1996ce073edd5fcc7ff
shichuyu <1042048096@qq.com>
2020-02-26 18:32:55 +0800

    配置vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,14 @@
  * @Author: shichuyu
  * @Date: 2020-01-16 16:29:26
  * @LastEditors: shichuyu
- * @LastEditTime: 2020-01-16 16:29:41
+ * @LastEditTime: 2020-02-26 17:56:46
  * @Description: 
  */
+const path = require('path');
+const devServerPort = 9527; // TODO: get this variable from setting.ts
+const mockServerPort = 9528; // TODO: get this variable from setting.ts
+
+const name = 'tywd'
 module.export = {
   // 选项
 }
