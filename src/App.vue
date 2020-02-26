548079634ef695296598bc811e215a1d80316d38
shichuyu <1042048096@qq.com>
2020-02-26 17:08:44 +0800

    改写App.vue格式为ts格式

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
+<!--
+ * @Author: shichuyu
+ * @Date: 2020-02-26 13:59:59
+ * @LastEditors: shichuyu
+ * @LastEditTime: 2020-02-26 16:33:44
+ * @Description: 
+ -->
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+  name: 'App',
+})
+export default class extends Vue {}
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
